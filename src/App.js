bbe664d2b70a8d14d7a7a3240ac1a4d266b66fd0
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-05-24 14:52:41 +0200

    ingredients to component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import './App.css';
+import Product from "./Product";
 
 function App() {
   return (
+
+      <>
+          <header></header>
     <main>
-        <article className="product">
-            <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
-                alt="Citroenen"
 
+        <Product
+            afbeelding="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
+            naam="Citroen"
+            tekst="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
+               Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
+               Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
             />
-            <h2 className="product-name">Citroen</h2>
-            <p className="product-description">
-                "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
-                Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
-                Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-            </p>
-            </article>
 
-        <article className="product">
+        <Product
+            afbeelding="https://www.ahealthylife.nl/wp-content/uploads/2017/06/limoen_voedingswaarde-768x499.jpg"
+            naam="Limoen"
+            tekst="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie).
+                 Limoenen zijn rond en kleiner dan citroenen.
+                 De schil is dun, vrij glad en groen."
+        />
+
+        <Product
+            afbeelding="https://verbakelparty.nl/wp-content/uploads/2017/08/download-46.jpg"
+            naam="Ijsblokjes"
+            tekst="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
+                 Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+        />
+
+
+{/*        <article className="product">
             <img src="https://www.ahealthylife.nl/wp-content/uploads/2017/06/limoen_voedingswaarde-768x499.jpg"
                  alt="Limoen"
 
@@ -40,8 +56,11 @@ function App() {
                 "Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
                  Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
             </p>
-        </article>
+        </article>*/}
         </main>
+
+
+          </>
   );
 }
 
---
+++ src/Product.js
@@ -0,0 +1,29 @@
+/*
+<article className="product">
+    <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
+         alt="Citroenen"
+
+    />
+    <h2 className="product-name">Citroen</h2>
+    <p className="product-description">
+        "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
+        Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
+        Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+    </p>
+</article>*/
+
+
+function Product({naam, tekst, afbeelding})
+
+{
+    return(
+        <article className="product">
+            <h2 className="product-name">{naam}</h2>
+            <p className="product-description">{tekst}</p>
+            <img src={afbeelding}/>
+        </article>
+
+)
+}
+
+export default Product
